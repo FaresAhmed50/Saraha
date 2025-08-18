ccdb962ee91b68a451cd7537f509e25ec6e9ee46
FaresAhmed50 <fares.ahmed15967@aiet.edu.eg>
2025-08-18 23:43:37 +0300

    edit the Db connection function

--- src/dbConnection/dbConnection.js
+++ src/dbConnection/dbConnection.js
@@ -1,14 +1,14 @@
 import mongoose from 'mongoose';
 
 
-const dbConnection = await mongoose.connect("mongodb://localhost:27017/saraha")
-    .then(() => {
-            console.log("MongoDB Connected!");
-        }
-    ).catch((err) => {
-        console.log(err);
-    });
-
-
+const dbConnection = async () => {
+    await mongoose.connect("mongodb://localhost:27017/saraha")
+        .then(() => {
+                console.log("MongoDB Connected!");
+            }
+        ).catch((err) => {
+            console.log(err);
+        });
+}
 
 export default dbConnection;
