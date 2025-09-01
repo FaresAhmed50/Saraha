3d7153febeffe83a673f46d5fe7bf347791eec46
FaresAhmed50 <fares.ahmed15967@aiet.edu.eg>
2025-09-02 01:39:01 +0300

    config the helmet middleware and limiter middleware and cros origin

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,18 +3,40 @@ import userRouter from "./modules/user/user.controller.js";
 import massageRouter from "./modules/massage/massage.controller.js";
 import authRouter from "./modules/auth/auth.controller.js";
 import {globalErrorHandler} from "./middlewares/errorHandler.middleware.js";
+import {rateLimit} from "express-rate-limit"
+import cors from "cors"
+import helmet from "helmet"
 
 
 const bootstrap = ({app , express}) => {
 
 
+    const wishList = [undefined];
+    const crosOptions = {
+        original : (origin , callback) => {
+            if(wishList.includes(origin )){
+                callback(null, true)
+            }else {
+                callback(new Error("cros not allowed."));
+            }
+        }
+    };
+
+    const limiter = rateLimit({
+        windowMs: 60*1000,
+        max: 5,
+        skipSuccessfulRequests: true
+    });
 
 
-
+    app.use(cors(crosOptions))
+    app.use(limiter);
+    app.use(helmet());
     app.use(express.json());
 
-    dbConnection();
 
+
+    dbConnection();
 
 
     app.use("/auth", authRouter);
